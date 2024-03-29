c6e89e5042d308c99a1da2eb5618f1840d06cd90
Sergey Kuzmin <s.kuzmin@mail.ru>
2024-03-29 23:59:02 +0800

    troubles

--- network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from network.models import SocialUser
-# Register your models here.
-
-
-@admin.register(SocialUser)
-class SocialUserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'date_of_birth', 'avatar', 'username', 'password')
--- network/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-
-class SocialUser(User):
-    date_of_birth = models.DateField(null=True, blank=True)
-    avatar = models.ImageField(upload_to='avatars', null=True, blank=True)
